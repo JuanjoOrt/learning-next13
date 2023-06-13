8c10a85f228977707789597b1b536dd2558e54d9
Juanjo Ortiz <juanjoseortizgamez@gmail.com>
2023-06-13 09:58:44 +0200

    status managed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import styles from './page.module.css'
 import { signOut, useSession } from 'next-auth/react'
 import LoginForm from '@/app/login/LoginForm'
 
+const STATUS_LOADING = 'loading'
+const STATUS_AUTHENTICATED = 'authenticated'
+const STATUS_UNAUTHENTICATED = 'unauthenticated'
 
 export default function Home() {
-  const { data: session } = useSession()
-  console.log(session)
+  const { data: session, status } = useSession()
 
   const handleSignOut = (e) => {
     e.preventDefault()
@@ -15,13 +17,14 @@ export default function Home() {
 
   return (
       <main className={styles.main}>
-        {(!session)
-          ? <LoginForm />
-          : <div>
-              <p>Hola {session.user?.name}</p>
-              <button onClick={handleSignOut}>sign out</button>
-            </div>
-        }
+        {status === STATUS_LOADING && <p>Loading...</p>}
+        {status === STATUS_UNAUTHENTICATED && <LoginForm />}
+        {status === STATUS_AUTHENTICATED && (
+          <div>
+            <p>Hola {session?.user?.name}</p>
+            <button onClick={handleSignOut}>sign out</button>
+          </div>
+        )}
       </main>
   )
 }
